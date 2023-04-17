cfe13a2e8257d2141f300c572ed04201bc611f71
dannycod3r <oseid7807@gmail.com>
2023-04-17 12:47:47 +0000

    Chore: Add header file to include all other headers created.
    This header file will be a source of truth.
    It will contain all other header files i.e.
    - utils.h
    - constants.h
    - libs.h
    and any other header files created later on

---
+++ simple_shell.h
@@ -0,0 +1,12 @@
+#ifndef _SIMPLE_SHELL_H
+#define _SIMPLE_SHELL_H
+/*simple_shell.h - includes all other header files*/
+
+#include "constants.h"
+#include "libs.h"
+#include "utils.h"
+
+/*program entry point*/
+int main(int ac, char **av, char **env);
+
+#endif /*_SIMPLE_SHELL_H*/
