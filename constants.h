ade4ccfcb3ac5dac3de02a545d8b97fd46f7e3aa
Prince Aboagye <aboagyeprince717@gmail.com>
2023-04-20 13:22:57 -0700

    Define constants to use

--- constants.h
+++ constants.h
@@ -1,5 +1,10 @@
 #ifndef _CONSTANTS_H
 #define _CONSTANTS_H
 /*constant.h - includes macros, constants and typedefs*/
+#define MAX_ARGS 10
+
+#define MAX_PATHS 1024
+
+#define BUFFER_SIZE 1024
 
 #endif /*_CONSTANTS_H*/
