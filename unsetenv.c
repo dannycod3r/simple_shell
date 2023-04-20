ad790a3c7b354001cefdf5158b69c38d6011b305
Prince Aboagye <aboagyeprince717@gmail.com>
2023-04-20 13:46:06 -0700

    Write unsetenv function to be implemented

---
+++ unsetenv.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * unset_env - unset environment variable
+ * @name: name of variable
+ *
+ * Return: 0 on success else -1
+ */
+int unset_env(char *name)
+{
+	int result = unsetenv(name);
+	
+	if (name == NULL || name[0] == '\0')
+	{
+		fprintf(stderr, "unsetenv: invalid argument\n");
+		return (-1);
+	}
+	if (result < 0)
+	{
+		fprintf(stderr, "unsetenv: failed to unset %s\n", name);
+		return (-1);
+	}
+	return (0);
+}
