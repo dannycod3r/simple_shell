aa1d7444fe5cd3c9102411cc102a0de1bcee0bab
dannycod3r <oseid7807@gmail.com>
2023-04-21 17:25:20 +0000

    Fix: Add function declaration for custom_getline in header file

--- utils.h
+++ utils.h
@@ -1,7 +1,7 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 /*utils.h - includes declarations for all helper functions*/
-
+int custom_getline(char **input, size_t *input_size, FILE *stream);
 
 
 #endif  /*_UTILS_H*/
